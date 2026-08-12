e86af9c4873d27332e8c2a336c5ad8122c10e253
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-08-12 22:15:45 +0530

    fix media serving

--- flyingfox/urls.py
+++ flyingfox/urls.py
@@ -27,5 +27,7 @@ urlpatterns = [
     path("", include("flyingfox_app.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
